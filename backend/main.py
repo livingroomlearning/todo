59bf80b44d3c1f22bb928d7c9cf1d68541cc5e3b
Olivia <none@digitaldebrief.com>
2023-08-22 07:20:32 +0000

    Display todo goals in react app.

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 import os
 import psycopg2
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, jsonify
 
 app = Flask(__name__, static_folder='../frontend/build')
 
@@ -20,12 +20,19 @@ def index():
 def frontend_assets(path):
     return send_from_directory(app.static_folder, os.path.join('static', path))
 
-@app.route('/api/todos')
+@app.route('/api/todos', methods=['GET'])
 def get_todos():
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('SELECT * FROM todos')
+    cur.execute('SELECT id, goal FROM todos')
     todos = cur.fetchall()
     cur.close()
     conn.close()
-    return {'todos': todos}
+    todos = [{'id': todo[0], 'goal': todo[1]} for todo in todos]
+    print({'todos': todos})
+    return jsonify({'todos': todos})
+
+
+@app.route('/<path:path>')
+def frontend_public(path):
+    return send_from_directory(app.static_folder, path)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react"
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  const fetchUserData = () => {
+    fetch("/api/todos", {method: "GET"})
+      .then(response => {
+        console.log(response)
+        return response.json()
+      })
+      .then(data => {
+        console.log(data)
+        setUsers(data.todos)
+      })
+  }
+
+  useEffect(() => {
+    fetchUserData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {users.length > 0 && (
+        <ul>
+          {users.map(user => (
+            <li key={user['id']}>{user['goal']}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
